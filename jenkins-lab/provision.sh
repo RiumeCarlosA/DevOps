ab68b423bb80ebca5b8cb0c3a2dc0c58c9fe51ce
RiumeCarlosA <riumecarlos@hotmail.com>
2023-05-27 08:28:03 +0100

    terminando lab de jenkins inicial

---
+++ jenkins-lab/provision.sh
@@ -0,0 +1,24 @@
+#bin/bash
+sudo yum install epel-release -y
+sudo yum install wget git -y
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install java-11-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+
+###instalação do docker e docker compose
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose   
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo chown -R root:docker /var/run/docker
+sudo usermod -aG docker jenkins
